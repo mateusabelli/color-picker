123b376555025eb389cd0f7956c61cd6ed6c8f60
AB3LL1 <43862225+AB3LL1@users.noreply.github.com>
2022-02-21 22:36:38 -0300

    Added new feature
    - Added a Dictionary class to provide string values for the meaning of the colors
    - Updated the Controller class to call the methods on the dictionary

--- src/main/java/com/example/app/Controller.java
+++ src/main/java/com/example/app/Controller.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+import java.lang.annotation.Documented;
+
 public class Controller {
 
     @FXML
@@ -27,13 +29,31 @@ public class Controller {
         String btnId = ((Control) event.getSource()).getId();
 
         switch (btnId) {
-            case "btnRed"    -> titleBox.setStyle("-fx-background-color: RED");
-            case "btnOrange" -> titleBox.setStyle("-fx-background-color: ORANGE");
-            case "btnYellow" -> { titleBox.setStyle("-fx-background-color: YELLOW");
-                titleLabel.setTextFill(Color.BLACK); }
-            case "btnGreen"  -> titleBox.setStyle("-fx-background-color: GREEN");
-            case "btnBlue"   -> titleBox.setStyle("-fx-background-color: BLUE");
-            case "btnPurple" -> titleBox.setStyle("-fx-background-color: PURPLE");
+            case "btnRed" -> {
+                titleBox.setStyle("-fx-background-color: RED");
+                infoLabel.setText(Dictionary.red());
+            }
+            case "btnOrange" -> {
+                titleBox.setStyle("-fx-background-color: ORANGE");
+                infoLabel.setText(Dictionary.orange());
+            }
+            case "btnYellow" -> {
+                titleBox.setStyle("-fx-background-color: YELLOW");
+                titleLabel.setTextFill(Color.BLACK);
+                infoLabel.setText(Dictionary.yellow());
+            }
+            case "btnGreen" -> {
+                titleBox.setStyle("-fx-background-color: GREEN");
+                infoLabel.setText(Dictionary.green());
+            }
+            case "btnBlue" -> {
+                titleBox.setStyle("-fx-background-color: BLUE");
+                infoLabel.setText(Dictionary.blue());
+            }
+            case "btnPurple" -> {
+                titleBox.setStyle("-fx-background-color: PURPLE");
+                infoLabel.setText(Dictionary.purple());
+            }
         }
     }
 }
---
+++ src/main/java/com/example/app/Dictionary.java
@@ -0,0 +1,23 @@
+package com.example.app;
+
+public class Dictionary {
+
+    public static String red() {
+        return  "Red, the color of blood and fire, is associated with meanings of love, passion, desire, heat, longing, lust, sexuality, sensitivity, romance, joy, strength, leadership, courage, vigor, willpower, rage, anger, danger, malice, wrath, stress, action, vibrancy, radiance, and determination.";
+    }
+    public static String orange() {
+        return  "Orange, the blend of red and yellow, is a mixture of the energy associated with red and the happiness associated with yellow. Orange is associated with meanings of joy, warmth, heat, sunshine, enthusiasm, creativity, success, encouragement, change, determination, health, stimulation, happiness, fun, enjoyment, balance, sexuality, freedom, expression, and fascination.";
+    }
+    public static String yellow() {
+        return  "Yellow, the color of sunshine, hope, and happiness, has conflicting associations. A dull or dingy yellow may represent caution, sickness, and jealousy. Studies show that the meaning of the color yellow can be warmth, cheerfulness, increased mental activity, increased muscle energy.";
+    }
+    public static String green() {
+        return  "Green, the color of life, renewal, nature, and energy, is associated with meanings of growth, harmony, freshness, safety, fertility, and environment. Green is also traditionally associated with money, finances, banking, ambition, greed, jealousy, and wall street.";
+    }
+    public static String blue() {
+        return  "Blue represents both the sky and the sea, and is associated with open spaces, freedom, intuition, imagination, expansiveness, inspiration, and sensitivity. Blue also represents meanings of depth, trust, loyalty, sincerity, wisdom, confidence, stability, faith, heaven, and intelligence.";
+    }
+    public static String purple() {
+        return  "Purple combines the calm stability of blue and the fierce energy of red. The color purple is often associated with royalty, nobility, luxury, power, and ambition. Purple also represents meanings of wealth, extravagance, creativity, wisdom, dignity, grandeur, devotion, peace, pride, mystery, independence, and magic.";
+    }
+}
